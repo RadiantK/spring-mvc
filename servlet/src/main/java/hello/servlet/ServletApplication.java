5e0d3606717a8476f75fa33d8a19ff0999c6ac20
RadiantK <pps8853@gmail.com>
2022-09-21 22:10:14 +0900

    application.properties파일 설정 로깅-debug 및 스프링 mvc viewResolver 설정

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,8 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan // 서블릿을 자동 등록하고 실행
 @SpringBootApplication
@@ -12,4 +14,10 @@ public class ServletApplication {
 		SpringApplication.run(ServletApplication.class, args);
 	}
 
+/*
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		return new InternalResourceViewResolver("/WEB-INF/view/", ".jsp");
+	}
+*/
 }
