95e171a55dd319ff4a4dde80a0217723438edb79
RadiantK <pps8853@gmail.com>
2022-09-25 01:02:40 +0900

    메시지 기능 및 국제화 적용 messages.properties파일을 사용한 메시지 기능 적용 및 messages_en.properties파일을 사용한 국제화 적용 확인

--- message/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ message/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,6 +2,7 @@ package hello.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class ItemServiceApplication {
@@ -9,5 +10,16 @@ public class ItemServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
+
+/*
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		// 프로퍼티 파일 명 messages.properties
+		messageSource.setBasenames("messages", "errors");
+		messageSource.setDefaultEncoding("utf-8");
+		return messageSource;
+	}
+*/
 
 }
